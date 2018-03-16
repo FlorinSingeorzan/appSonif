572bc6d23c1add2e2ca9a45593ea9e459883ea02
z003u79s <z003u79s@EVR01107NB.ad001.siemens.net>
2018-03-16 14:19:11 +0200

    Commit - separate shell

--- PrjTest/WebContent/webapp/controller/Toolbar.controller.js
+++ PrjTest/WebContent/webapp/controller/Toolbar.controller.js
@@ -1,3 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/format/NumberFormat"
+], function(Controller, MessageToast, JSONModel, NumberFormat) {    //tine cont de ordinea de sus
+	"use strict";
+
 sap.ui.controller("sap.suite.ui.commons.PrjTest.controller.Toolbar", {
 
 //	onInit: function() {
@@ -7,6 +15,9 @@ sap.ui.controller("sap.suite.ui.commons.PrjTest.controller.Toolbar", {
 		this.getOwnerComponent().getRouter().navTo("home");
 //		window.location.reload();
 //		oList.getModel().updateBindings();
+	},
+	toggleShell : function (){
+		MessageToast.show(this.getView());
 	}
-
 });
+});  
