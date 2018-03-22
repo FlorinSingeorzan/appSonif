5d370a8c4a1a82c67155cfcb0062dbf31e73e622
z003u7by <z003u7by@EVR01111NB.ad001.siemens.net>
2018-03-22 16:13:23 +0200

    popup on charts

--- PrjTest/WebContent/webapp/controller/ChartContainer.controller.js
+++ PrjTest/WebContent/webapp/controller/ChartContainer.controller.js
@@ -7,6 +7,27 @@ sap.ui.define([
 		onNavButtonPressed: function() {
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.navTo("home");
+		},
+		
+		onClick: function(oEvent) {
+			//var oNode = oEvent.getParameters();
+			var sPath = oNode.getBindingContext().getPath() + "/quickChartView";
+			
+			if (!this.oQuickView) {
+		
+				this.oQuickView = sap.ui.xmlfragment("sap.suite.ui.commons.PrjTest.fragment.QuickChartView", this);
+				this.getView().addDependent(this.oQuickView);
+				
+			}
+			this.oQuickView.bindElement(sPath);
+			this.oQuickView.open();
+		},
+
+		onExit: function () {
+			if (this.oQuickView) {
+				this.oQuickView.destroy();
+			}
 		}
+	
 	});
 });
