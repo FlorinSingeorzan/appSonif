2e15728c52d9d9b3e0c4bcfe68816e5cf9876c83
Timea <Timea@DESKTOP-PVTBPGJ>
2018-03-29 22:51:59 +0300

    mining tech details swiping images

--- PrjTest/WebContent/webapp/controller/ProcessFlow.controller.js
+++ PrjTest/WebContent/webapp/controller/ProcessFlow.controller.js
@@ -12,45 +12,130 @@ sap.ui.define([
 			this.getOwnerComponent().getRouter().navTo("home");
 		},
 		
-		onInit: function () {
-			var sDataPath = jQuery.sap.getModulePath("sap.suite.ui.commons.PrjTest.model.data", "/ProcessFlowData.json");
-			var oModel = new JSONModel(sDataPath);
-			this.getView().setModel(oModel);
+		onInit: function (evt) {
+			// set explored app's demo model on this sample
+			var oImgModel = new JSONModel(jQuery.sap.getModulePath("sap.suite.ui.commons.PrjTest.model.data", "/ProcessFlowData.json"));
+			this.getView().setModel(oImgModel);
 
-			this.oProcessFlow = this.getView().byId("processflow");
-			this.oProcessFlow.updateModel();
+			// set the possible screen sizes
+			var oCarouselContainer = {
+				screenSizes : [
+					"350px",
+					"420px",
+					"555px",
+					"743px",
+					"908px"
+				]
+			};
+			var oScreenSizesModel = new JSONModel(oCarouselContainer);
+			this.getView().setModel(oScreenSizesModel, "ScreenSizesModel");
+
+			this._setNumberOfImagesInCarousel(3);
 		},
-
-		onZoomIn: function () {
-			this.oProcessFlow.zoomIn();
-
-			MessageToast.show("Zoom level changed to: " + this.oProcessFlow.getZoomLevel());
-		},
-
-		onZoomOut: function () {
-			this.oProcessFlow.zoomOut();
-
-			MessageToast.show("Zoom level changed to: " + this.oProcessFlow.getZoomLevel());
-		},
-
-		onNodePress: function(oEvent) {
-			var oNode = oEvent.getParameters();
-			var sPath = oNode.getBindingContext().getPath() + "/quickView";
-			
-			if (!this.oQuickView) {
-		
-				this.oQuickView = sap.ui.xmlfragment("sap.suite.ui.commons.PrjTest.fragment.QuickView", this);
-				this.getView().addDependent(this.oQuickView);
-				
+		onArrowsPlacementSelect: function (oEvent) {
+			var oCarousel = this.getView().byId("carouselSample");
+			var sSelectedValue = oEvent.getSource().getSelectedButton().getText();
+			if (sSelectedValue === "Content") {
+				oCarousel.setArrowsPlacement(sap.m.CarouselArrowsPlacement.Content);
+			} else if (sSelectedValue === "PageIndicator") {
+				oCarousel.setArrowsPlacement(sap.m.CarouselArrowsPlacement.PageIndicator);
 			}
-			this.oQuickView.bindElement(sPath);
-			this.oQuickView.openBy(oNode);
 		},
+		onPageIndicatorPlacementSelect: function (oEvent) {
+			var oCarousel = this.getView().byId("carouselSample");
+			var sSelectedValue = oEvent.getSource().getSelectedButton().getText();
+			if (sSelectedValue === "Bottom") {
+				oCarousel.setPageIndicatorPlacement(sap.m.PlacementType.Bottom);
+			} else if (sSelectedValue === "Top") {
+				oCarousel.setPageIndicatorPlacement(sap.m.PlacementType.Top);
+			}
+		},
+		onShowPageIndicatorSelect: function (oEvent) {
+			var oCarousel = this.getView().byId("carouselSample");
+			var sSelectedValue = oEvent.getSource().getSelectedButton().getText();
+			if (sSelectedValue === "Yes") {
+				oCarousel.setShowPageIndicator(true);
+			} else if (sSelectedValue === "No") {
+				oCarousel.setShowPageIndicator(false);
+			}
+		},
+		onSliderMoved: function (oEvent) {
+			var origingalHeight = 650;
+
+			var screenSizesJSON = this.getView().getModel("ScreenSizesModel").getData();
+			var iValue = oEvent.getParameter("value");
+			var screenWidth = screenSizesJSON.screenSizes[Number(iValue) - 1];
+			var oCarouselContainer = this.getView().byId("carouselContainer");
+			oCarouselContainer.setWidth(screenWidth);
+			var screenHeight = origingalHeight * parseFloat(screenWidth) / 1000;
+			oCarouselContainer.setHeight(screenHeight + 'px');
+		},
+		onNumberOfImagesChange: function (oEvent) {
+			var numberOfImages = oEvent.getSource().getValue();
+			this._setNumberOfImagesInCarousel(Number(numberOfImages));
+		},
+		_setNumberOfImagesInCarousel: function (numberOfImages) {
+			if (!numberOfImages || numberOfImages < 1 || numberOfImages > 9){
+				return;
+			}
+
+			var oCarousel = this.getView().byId("carouselSample");
+			oCarousel.destroyPages();
+
+			for (var i = 0; i < numberOfImages; i++) {
+				switch (i+1){
+				case 1:
+					var imgSrc = "images/ProcessFlowImageContent/1.jpg";
+					var imgAlt = "Example picture " + (i + 1);
+					var img = new sap.m.Image( {
+						src: imgSrc,
+						alt: imgAlt,
+						densityAware: false,
+						decorative: false
+					});
+					oCarousel.addPage(img);
+				case 2:
+					var imgSrc = "images/ProcessFlowImageContent/2.jpg";
+					var imgAlt = "Example picture " + (i + 1);
+					var img = new sap.m.Image( {
+						src: imgSrc,
+						alt: imgAlt,
+						densityAware: false,
+						decorative: false
+					});
+					oCarousel.addPage(img);
+				case 3:
+					var imgSrc = "images/ProcessFlowImageContent/3.jpg";
+					var imgAlt = "Example picture " + (i + 1);
+					var img = new sap.m.Image( {
+						src: imgSrc,
+						alt: imgAlt,
+						densityAware: false,
+						decorative: false
+					});
+					oCarousel.addPage(img);
+				}
+/*
+				var imgId = "pic" + (i + 1);
+				var imgSrc = "{sap.suite.ui.commons.TestPopUpChart.model.data.ProcessFlowData>/images/" + i + "}";
+				var imgAlt = "Example picture " + (i + 1);
+				var img = new sap.m.Image(imgId, {
+					src: imgSrc,
+					alt: imgAlt,
+					densityAware: false,
+					decorative: false
+				});
+*/
+				oCarousel.addPage(img);
+			}
+		},
+
 
 		onExit: function () {
 			if (this.oQuickView) {
 				this.oQuickView.destroy();
 			}
 		}
+		
 	});
 });
