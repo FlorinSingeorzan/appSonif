cd64ca40e6a20ffc65603dd8799975929dbf58f8
yonut <yonutz_qx3@yahoo.com>
2018-04-02 21:42:36 +0300

    Converter

--- PrjTest/WebContent/webapp/controller/Convertor.controller.js
+++ PrjTest/WebContent/webapp/controller/Convertor.controller.js
@@ -9,6 +9,20 @@ sap.ui.define([
 	return Controller.extend("sap.suite.ui.commons.PrjTest.controller.Convertor", {
 		onInit: function() {  
 		},
-		
+		dollarChange: function (oEvent) {
+			var dollar= this.byId("dollar").getValue();
+			this.byId("bitcoin").setValue(dollar/7033);
+			this.byId("ethereum").setValue(dollar/387);
+		},
+		bitcoinChange: function (oEvent) {
+			var bitcoin= this.byId("bitcoin").getValue();
+			this.byId("dollar").setValue(bitcoin*7033);
+			this.byId("ethereum").setValue(bitcoin*18);
+		},
+		ethereumChange: function (oEvent) {
+			var ethereum= this.byId("ethereum").getValue();
+			this.byId("dollar").setValue(ethereum/378);
+			this.byId("bitcoin").setValue(ethereum/18.2);
+		}
 	});
 });  
