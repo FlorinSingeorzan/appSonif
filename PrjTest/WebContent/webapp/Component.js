3d849fcf266cd072ab592257c8e4efebabf226dd
yonut <yonutz_qx3@yahoo.com>
2018-04-16 00:07:38 +0300

    models.js removed

--- PrjTest/WebContent/webapp/Component.js
+++ PrjTest/WebContent/webapp/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/suite/ui/commons/PrjTest/model/models",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device"
-], function(UIComponent, models,JSONModel,Device) {
+], function(UIComponent,JSONModel,Device) {
 	"use strict";
 
 	return UIComponent.extend("sap.suite.ui.commons.PrjTest.Component", {
@@ -12,17 +11,9 @@ sap.ui.define([
 			manifest: "json"
 		},
 
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
 
 			// create the views based on the url/hash
 			this.getRouter().initialize();
